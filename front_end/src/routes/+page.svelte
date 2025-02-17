0f69038715ef5abec3411184b64b34da86b96950
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2025-02-17 02:42:47 +0000

    feat: adding more links to the homepage
    Signed-off-by: Andrew Steurer <94206073+asteurer@users.noreply.github.com>

--- front_end/src/routes/+page.svelte
+++ front_end/src/routes/+page.svelte
@@ -1,18 +1,32 @@
 <h1>Andrew Steurer</h1>
 <h4>Back-end and infrastructure engineer</h4>
-<p>Welcome! Feel free to use the navigation above or the links below:</p>
+
+<h3>Links</h3>
 <ul>
     <li><a href="https://github.com/asteurer/asteurer.com">Click here</a> to see the source code and technical details of this website</li>
     <li><a href="https://linkedin.com/in/asteurer">Click here</a> for my LinkedIn</li>
     <li><a href="https://github.com/asteurer">Click here</a> for my Github</li>
 </ul>
 
+<h3>Blog Posts About Code I Wrote</h3>
+<ul>
+  <li><a href="https://www.fermyon.com/blog/spinkube-on-kairos">Using SpinKube on Kairos</a></li>
+  <li><a href="https://fermyon.com/blog/api-testing-demo">Automating API endpoint testing with Spin</a></li>
+  <li><a href="https://www.fermyon.com/blog/using-azure-and-spin">Integrating Spin with Azure services</a></li>
+  <li><a href="https://www.fermyon.com/blog/otel-plugin">Telemetry for Spin Apps with the OTel Plugin</a></li>
+  <li><a href="https://www.fermyon.com/blog/verman-plugin">Spin Verman - The Version Manager Plugin</a></li>
+</ul>
+
 <style>
     h1 {
-      margin-bottom: 0rem; /* Reduce bottom margin of h1 */
+      margin-bottom: 0rem; /* Reduce bottom margin */
+    }
+
+    h3 {
+      margin-bottom: 0rem; /* Reduce bottom margin */
     }
 
     h4 {
-      margin-top: 0rem; /* Reduce top margin of h4 */
+      margin-top: 0rem; /* Reduce top margin*/
     }
   </style>
